6827d9f470527f95603aad16104c25451dda937d
Jakov Lovrić <jakov.lovric@velebit.ai>
2023-05-08 16:37:09 +0200

    Add Colors to data utils

--- color_tagging/utils/data_utils.py
+++ color_tagging/utils/data_utils.py
@@ -5,10 +5,14 @@ CLASSES_INT = {
 INT_CLASS = {
     0: "dress", 1: "shirt"}
 
-# RGB order
-MASK_PALETTE = [(255, 255, 255), (17, 119, 51), (136, 34, 85)]
+COLORS = ["red", "blue"]
+INT_CLASS = {
+    0: "red", 1: "blue"}
 
 # BGR order
 COLORS = {"Skip": (255, 255, 0),
           "dress": (51, 119, 17),
           "shirt": (85, 34, 136)}
+
+# RGB order
+MASK_PALETTE = [(255, 255, 255), (17, 119, 51), (136, 34, 85)]
